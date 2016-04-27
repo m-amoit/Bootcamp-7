707326b6eadf99f79fcab52be5919606c1b42b5d
Miriam Amoit <amoitjmiriam@gmail.com>
2016-04-27 10:24:50 +0300

    Add data_types.py

---
+++ day_3/data_types.py
@@ -0,0 +1,24 @@
+def data_type(x):
+	'''
+	Takes in an argument, x:
+		- For a integer, return x**2
+		- For a float, return x/2
+		- For a string, return "hello" + x
+		- For a boolean, return "boolean"
+		- For a long, return "long"
+	'''
+	if type(x) == int:
+		return x**2
+	elif type(x) == float:
+		return x/2
+	elif type(x) == str:
+		return 'hello ' + x
+	elif type(x) == bool:
+		return "boolean"
+	elif type(x) == long:
+		return "long"
+	else:
+		pass
+
+
+
