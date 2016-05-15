1df2f29e0623be2d81bae2cb62fcac704362fc4d
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-15 10:36:08 +0300

    Using regex

--- andelabs/word_count.py
+++ andelabs/word_count.py
@@ -2,15 +2,22 @@ import re
 
 def words(A):
 	count = dict ()
-	for i in A.split():
-		if i is '':
-			continue
-		else:
-			if i not in count:
-				count[i] = 1
-			else:
-				count[i] = count [i] + 1
+	x = re.findall('\S+', A)
+	for i in x:
+		count[i] = count.get(i, 0) + 1
+
 	return count
+
+	
+	# for i in A.split():
+	# 	if i is '':
+	# 		continue
+	# 	else:
+	# 		if i not in count:
+	# 			count[i] = 1
+	# 		else:
+	# 			count[i] = count [i] + 1
+	# return count
 
 
 		
