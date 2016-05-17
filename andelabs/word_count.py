2323172b08ce11b808b67b04ff4d33d59952c645
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-17 14:23:16 +0300

    Using regex

--- andelabs/word_count.py
+++ andelabs/word_count.py
@@ -2,7 +2,7 @@ import re
 
 def words(A):
 	count = dict ()
-	x = re.findall('\S+', A)
+	x = re.findall('\S+', A) # All none whitespace characters, one or more
 	for i in x:
 		count[i] = count.get(i, 0) + 1
 
