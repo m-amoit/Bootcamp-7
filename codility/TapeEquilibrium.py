9cb8af4e54885b2639da577ac1c2a4dddff8a99c
Miriam Amoit <amoitjmiriam@gmail.com>
2016-06-01 13:35:39 +0300

    final TapeEquilibrium: 100%

--- codility/TapeEquilibrium.py
+++ codility/TapeEquilibrium.py
@@ -2,8 +2,9 @@ def solution(A):
     total = A[0]
     y = sum(A)
     result = abs(2*total - y)
-    for P in range(1, len(A)):
+    for P in range(1, len(A)-1):
         total += A[P]
         diff = abs(2*total - y)
         result = min(diff, result)
     return result
+
